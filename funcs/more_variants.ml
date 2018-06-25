466f0fea75686ba4c7b4c26698d155046984191e
kuroskira <pat.halo@hotmail.it>
2018-06-25 19:28:54 +0200

    mh

--- funcs/more_variants.ml
+++ funcs/more_variants.ml
@@ -1,27 +1,28 @@
-type point = float * float
-type vector = float list
-type matrix = float list list
+type point = float * float;;
+type vector = float list;;
+type matrix = float list list;;
 
-let pi = acos(-. 1.)
+(* let pi = acos(-. 1.) *)
 
 let getx : point -> float =
   fun (x,_) -> x
 ;;
 
-let pt : point = (1.,2.)
-let flatpair : float*float = (1.,3.)
+let pt : point = (1.,2.);;
+let flatpair : float*float = (1.,3.);;
 
-let one = getx pt
-let one' = getx flatpair
+let one = getx pt;;
+let one' = getx flatpair;;
 
 type shape =
   | Point of point
   | Circle of point * float (* Center and radius *)
   | Rect of point * point (* Lower left and upper-right corners *)
+;;
 
 let area = function
-  | Point _ -> 0.0
-  | Circle (_,r) -> pi *. (r ** 2.0)
+  | Point _      -> 0.0
+  | Circle (_,r) -> Float.pi *. (r ** 2.0)
   | Rect ((x1,y1),(x2,y2)) -> 
     let w = x2 -. x1 in
     let h = y2 -. y1 in
@@ -29,9 +30,9 @@ let area = function
 ;;
 
 let center = function
-  | Point p -> p
+  | Point   p    -> p
   | Circle (p,_) -> p
-  | Rect ((x1,y1),(x2,y2)) ->
+  | Rect   ((x1,y1),(x2,y2)) ->
     ((x2 -. x1) /. 2.0,
       (y2 -. y1) /. 2.0)
 ;;
@@ -46,7 +47,7 @@ type string_or_int_list = string_or_int list
 let rec sum : string_or_int_list -> int = function
   | [] -> 0
   | (String s)::t -> int_of_string s + sum t
-  | (Int i)::t -> i + sum t
+  | (Int i)::t    -> i + sum t
 ;;
 
 let three = sum [String "2"; Int 1];;
